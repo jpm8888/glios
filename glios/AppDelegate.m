e1c4179862b415f157a0774d7ad5a0345d033ea9
Psi Gem <psigem@psis-mac-mini.local>
2017-04-05 12:28:16 +0530

    Changes

--- glios/AppDelegate.m
+++ glios/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Hello World
     // Override point for customization after application launch.
     return YES;
 }
