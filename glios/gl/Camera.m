2e71742ad81122e031ead9f8d045cc3fb72e3a96
jpm8888 <jpm8888@gmail.com>
2017-04-11 16:10:39 +0530

    removed comments

--- glios/gl/Camera.m
+++ glios/gl/Camera.m
@@ -16,17 +16,6 @@
     GLKQuaternion tmp2;
 }
 
-//private final Ray ray = new Ray(new Vector3(), new Vector3());
-
-
-/** Recalculates the projection and view matrix of this camera and the {@link Frustum} planes. Use this after you've manipulated
- * any of the attributes of the camera. */
-//public abstract void update ();  ////*************////***********////
-
-/** Recalculates the projection and view matrix of this camera and the {@link Frustum} planes if <code>updateFrustum</code> is
- * true. Use this after you've manipulated any of the attributes of the camera. */
-//public abstract void update (boolean updateFrustum); ////*************////***********////
-
 -(instancetype) init {
     self = [super init];
     if (self) {
@@ -43,10 +32,6 @@
     return self;
 }
 
-/** Recalculates the direction of the camera to look at the point (x, y, z). This function assumes the up vector is normalized.
- * @param x the x-coordinate of the point to look at
- * @param y the y-coordinate of the point to look at
- * @param z the z-coordinate of the point to look at */
 -(void) lookAt :(float) x :(float) y :(float) z {
     tmpVec = GLKVector3Make(0, 0, 0);
     tmpVec = GLKVector3Subtract(tmpVec, _position);
@@ -64,15 +49,10 @@
     }
 }
 
-
-/** Recalculates the direction of the camera to look at the point (x, y, z).
- * @param target the point to look at */
 -(void) lookAt :(GLKVector3) target {
     [self lookAt :target.x :target.y :target.z];
 }
 
-/** Normalizes the up vector by first calculating the right vector via a cross product between direction and up, and then
- * recalculating the up vector via a cross product between right and direction. */
 -(void) normalizeUp  {
     tmpVec = _direction;
     tmpVec = GLKVector3CrossProduct(tmpVec, _up);
@@ -82,13 +62,6 @@
     _up = GLKVector3Normalize(_up);
 }
 
-/** Rotates the direction and up vector of this camera by the given angle around the given axis. The direction and up vector
- * will not be orthogonalized.
- *
- * @param angle the angle
- * @param axisX the x-component of the axis
- * @param axisY the y-component of the axis
- * @param axisZ the z-component of the axis */
 -(void) rotate :(float) angle :(float) axisX :(float) axisY :(float) axisZ {
     tmpMat = GLKMatrix4Identity;
     tmpMat = GLKMatrix4Rotate(tmpMat, angle, axisX, axisY, axisZ);
@@ -96,11 +69,6 @@
     self.up = GLKMatrix4MultiplyVector3(tmpMat, self.direction);
 }
 
-/** Rotates the direction and up vector of this camera by the given angle around the given axis. The direction and up vector
- * will not be orthogonalized.
- *
- * @param axis the axis to rotate around
- * @param angle the angle */
 -(void) rotate :(GLKVector3) axis :(float) angle {
     tmpMat = GLKMatrix4Identity;
     tmpMat = GLKMatrix4RotateWithVector3(tmpMat, angle, axis);
@@ -108,20 +76,11 @@
     self.up = GLKMatrix4MultiplyVector3(tmpMat, self.up);
 }
 
-/** Rotates the direction and up vector of this camera by the given rotation matrix. The direction and up vector will not be
- * orthogonalized.
- *
- * @param transform The rotation matrix */
 -(void) rotate :(const GLKMatrix4) transform {
-    
     direction.rot(transform);
     up.rot(transform);
 }
 
-/** Rotates the direction and up vector of this camera by the given {@link Quaternion}. The direction and up vector will not be
- * orthogonalized.
- *
- * @param quat The quaternion */
 -(void) rotateQuat :(GLKQuaternion) quat {
 //    quat.transform(direction);
     quat = [self transform:self.direction];
@@ -129,24 +88,6 @@
     quat = [self transform:self.up];
 }
 
-//-(GLKVector3) transform :(GLKVector3) v {
-//    tmp2 = self;
-//    tmp2 = GLKQuaternionConjugate(tmp2);
-//    tmp1 = GLKQuaternionMake(v.x, v.y, v.z, 0);
-//    tmp2 = GLKQuaternionMultiply(tmp1, self);
-//    
-//    v.x = tmp2.x;
-//    v.y = tmp2.y;
-//    v.z = tmp2.z;
-//    return v;
-//}
-
-/** Rotates the direction and up vector of this camera by the given angle around the given axis, with the axis attached to given
- * point. The direction and up vector will not be orthogonalized.
- *
- * @param point the point to attach the axis to
- * @param axis the axis to rotate around
- * @param angle the angle */
 -(void) rotateAround :(GLKVector3) point :(GLKVector3) axis :(float) angle {
     tmpVec = point;
     tmpVec = GLKVector3Subtract(tmpVec, _position);
@@ -156,40 +97,14 @@
     [self translate :-tmpVec.x :-tmpVec.y :-tmpVec.z];
 }
 
-/** Transform the position, direction and up vector by the given matrix
- *
- * @param transform The transform matrix */
-//-(void) transform :(const GLKMatrix4) transform {
-//    _position = GLKMatrix4MultiplyVector3(transform, _position);
-//    [self rotate :transform];
-//}
-
-/** Moves the camera by the given amount on each axis.
- * @param x the displacement on the x-axis
- * @param y the displacement on the y-axis
- * @param z the displacement on the z-axis */
 -(void) translate :(float) x :(float) y :(float) z {
     _position = GLKVector3Add(_position, GLKVector3Make(x, y, z));
 }
 
-/** Moves the camera by the given vector.
- * @param vec the displacement vector */
 -(void) translate :(GLKVector3) vec {
     _position = GLKVector3Add(_position, vec);
 }
 
-/** Function to translate a point given in screen coordinates to world space. It's the same as GLU gluUnProject, but does not
- * rely on OpenGL. The x- and y-coordinate of vec are assumed to be in screen coordinates (origin is the top left corner, y
- * pointing down, x pointing to the right) as reported by the touch methods in {@link Input}. A z-coordinate of 0 will return a
- * point on the near plane, a z-coordinate of 1 will return a point on the far plane. This method allows you to specify the
- * viewport position and dimensions in the coordinate system expected by {@link GL20#glViewport(int, int, int, int)}, with the
- * origin in the bottom left corner of the screen.
- * @param screenCoords the point in screen coordinates (origin top left)
- * @param viewportX the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportY the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportWidth the width of the viewport in pixels
- * @param viewportHeight the height of the viewport in pixels
- * @return the mutated and unprojected screenCoords {@link Vector3} */
 -(GLKVector3) unproject :(GLKVector3) screenCoords :(float) viewportX :(float) viewportY :(float) viewportWidth :(float) viewportHeight {
     float x = screenCoords.x, y = screenCoords.y;
     x = x - viewportX;
@@ -202,40 +117,16 @@
     return screenCoords;
 }
 
-/** Function to translate a point given in screen coordinates to world space. It's the same as GLU gluUnProject but does not
- * rely on OpenGL. The viewport is assumed to span the whole screen and is fetched from {@link Graphics#getWidth()} and
- * {@link Graphics#getHeight()}. The x- and y-coordinate of vec are assumed to be in screen coordinates (origin is the top left
- * corner, y pointing down, x pointing to the right) as reported by the touch methods in {@link Input}. A z-coordinate of 0
- * will return a point on the near plane, a z-coordinate of 1 will return a point on the far plane.
- * @param screenCoords the point in screen coordinates
- * @return the mutated and unprojected screenCoords {@link Vector3} */
 -(GLKVector3) unproject :(GLKVector3) screenCoords {
     screenCoords = [self project :screenCoords :0 :0 :[[UIScreen mainScreen] bounds].size.width:[[UIScreen mainScreen] bounds].size.height];
     return screenCoords;
 }
 
-
-/** Projects the {@link Vector3} given in world space to screen coordinates. It's the same as GLU gluProject with one small
- * deviation: The viewport is assumed to span the whole screen. The screen coordinate system has its origin in the
- * <b>bottom</b> left, with the y-axis pointing <b>upwards</b> and the x-axis pointing to the right. This makes it easily
- * useable in conjunction with {@link Batch} and similar classes.
- * @return the mutated and projected worldCoords {@link Vector3} */
 -(GLKVector3) project :(GLKVector3) worldCoords {
     worldCoords = [self project:worldCoords :0 :0 :[[UIScreen mainScreen] bounds].size.width:[[UIScreen mainScreen] bounds].size.height];
     return worldCoords;
 }
 
-/** Projects the {@link Vector3} given in world space to screen coordinates. It's the same as GLU gluProject with one small
- * deviation: The viewport is assumed to span the whole screen. The screen coordinate system has its origin in the
- * <b>bottom</b> left, with the y-axis pointing <b>upwards</b> and the x-axis pointing to the right. This makes it easily
- * useable in conjunction with {@link Batch} and similar classes. This method allows you to specify the viewport position and
- * dimensions in the coordinate system expected by {@link GL20#glViewport(int, int, int, int)}, with the origin in the bottom
- * left corner of the screen.
- * @param viewportX the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportY the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportWidth the width of the viewport in pixels
- * @param viewportHeight the height of the viewport in pixels
- * @return the mutated and projected worldCoords {@link Vector3} */
 -(GLKVector3) project :(GLKVector3) worldCoords :(float) viewportX :(float) viewportY :(float) viewportWidth :(float) viewportHeight {
     worldCoords = GLKMatrix4MultiplyAndProjectVector3(_combined, worldCoords);
     worldCoords.x = viewportWidth * (worldCoords.x + 1) / 2 + viewportX;
@@ -244,14 +135,6 @@
     return worldCoords;
 }
 
-/** Creates a picking {@link Ray} from the coordinates given in screen coordinates. It is assumed that the viewport spans the
- * whole screen. The screen coordinates origin is assumed to be in the top left corner, its y-axis pointing down, the x-axis
- * pointing to the right. The returned instance is not a new instance but an internal member only accessible via this function.
- * @param viewportX the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportY the coordinate of the bottom left corner of the viewport in glViewport coordinates.
- * @param viewportWidth the width of the viewport in pixels
- * @param viewportHeight the height of the viewport in pixels
- * @return the picking Ray. */
 -(Ray*) getPickRay :(float) screenX :(float) screenY :(float) viewportX :(float) viewportY :(float) viewportWidth :(float) viewportHeight {
     ray.origin = GLKVector3Make(screenX, screenY, 0);
     [self unproject:ray.origin :viewportX :viewportY :viewportWidth :viewportHeight];
@@ -262,10 +145,6 @@
     return ray;
 }
 
-/** Creates a picking {@link Ray} from the coordinates given in screen coordinates. It is assumed that the viewport spans the
- * whole screen. The screen coordinates origin is assumed to be in the top left corner, its y-axis pointing down, the x-axis
- * pointing to the right. The returned instance is not a new instance but an internal member only accessible via this function.
- * @return the picking Ray. */
 -(Ray*) getPickRay :(float) screenX :(float) screenY {
     return [self getPickRay :screenX :screenY :0 :0 :[[UIScreen mainScreen] bounds].size.width :[[UIScreen mainScreen] bounds].size.height];
 }
